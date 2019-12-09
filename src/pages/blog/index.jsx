87e359a206fd3755ea8f469fc7eb9ea3acda7dd8
Aleš Chromec <chromec.ales@gmail.com>
2019-12-09 13:21:53 +0100

    Fix build V2

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Layout from '../../components/Layout'
+import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 import Navbar from '../../components/Navbar'
 import { BodyWrap } from '../../helpers/common'
